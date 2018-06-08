ffea517ae90612d4de8eb0ec3ed5164d09680fd1
Rob Rowe <rrowe@foodlogiq.com>
2018-06-08 13:39:05 -0400

    added summary filler for the first post

--- Conspiracies.elm
+++ Conspiracies.elm
@@ -7,17 +7,25 @@ import Html.Events exposing (onClick)
   
 view model =
     div [ class "container-fluid" ]
-        [ div  [ class "row" ] [
-         div [ 
-            class "col-md-2 d-none d-md-block bg-light sidebar"
-            ]
-            
-            (List.map (viewTag model.selectedTag) model.tags)
-        , div [ class "col-md-9 ml-sm-auto col-lg-10 px-4" ]
-            [ div [ class "conspicies" ] 
-               [ h2 [] [ text (model.selectedTag ++ " Conspiracies") ]]]
-        ]]
+        [ div  [ class "row" ] 
+          [
+            div [ class "col-md-2 d-none d-md-block bg-light sidebar"]
+             (List.map (viewTag model.selectedTag) model.tags)
+           ,div [ class "col-md-9 ml-sm-auto col-lg-10 px-4" ]
+              [ div [ class "content-heading" ] [ h2 [] [ text (model.selectedTag ++ " Conspiracies") ]]
+              , div [] (List.map (viewSummaries) model.summaryPlaceholders)
+              ]
+          ]
+        ]
  
+viewSummaries summary = 
+    div [ class "conspiracy-tease"]
+        [ div [ class "conspiracy-title"] 
+            [ text summary.title ]
+        ,div [ class "conspiracy-summary"] 
+            [ p [] [text summary.summary ]]
+        ,div [] [ text "View More Link Here"]    
+        ]
  
 viewTag selectedTag tag =
     div
@@ -34,6 +42,17 @@ initialModel =
               , "New World Order"
               , "UFO"]       
      , selectedTag = "All"
+     , summaryPlaceholders = [
+         { title = "Conspiracy Title Placeholder"
+           ,summary = "Concept of the number one, rogue with pretty stories for which there's little good evidence from which we spring Hypatia a mote of dust suspended in a sunbeam hydrogen atoms take root and flourish gathered by gravity Hypatia! Vangelis extraplanetary made in the interiors of collapsing stars vanquish the impossible! Birth galaxies. Inconspicuous motes of rock and gas Tunguska event, Orion's sword trillion! Worldlets vastness is bearable only through love rich in heavy atoms as a patch of light tesseract and billions upon billions upon billions upon billions upon billions upon billions upon billions!"
+         }
+         ,{ title = "Conspiracy Title Placeholder"
+            ,summary = "Emerged into consciousness intelligent beings, science the sky calls to us the ash of stellar alchemy laws of physics, dream of the mind's eye. Something incredible is waiting to be known billions upon billions decipherment not a sunrise but a galaxyrise descended from astronomers radio telescope concept of the number one muse about Euclid tesseract billions upon billions, preserve and cherish that pale blue dot intelligent beings tingling of the spine Sea of Tranquility Hypatia. Globular star cluster rich in mystery culture descended from astronomers ship of the imagination Apollonius of Perga. Birth Apollonius of Perga. Dispassionate extraterrestrial observer. How far away and billions upon billions upon billions upon billions upon billions upon billions upon billions!"
+         }
+         ,{ title = "Conspiracy Title Placeholder"
+            ,summary = "Colonies, hydrogen atoms Flatland tingling of the spine quasar. Billions upon billions! As a patch of light decipherment consciousness permanence of the stars cosmic fugue brain is the seed of intelligence from which we spring astonishment science, take root and flourish explorations! Apollonius of Perga intelligent beings. Cambrian explosion? Consciousness, network of wormholes. Intelligent beings a mote of dust suspended in a sunbeam encyclopaedia galactica, Euclid laws of physics the only home we've ever known a billion trillion? A mote of dust suspended in a sunbeam, great turbulent clouds Vangelis with pretty stories for which there's little good evidence brain is the seed of intelligence hearts of the stars and billions upon billions upon billions upon billions upon billions upon billions upon billions."
+         }
+     ]
     }
  
 update msg model =
--- elm.js
+++ elm.js
@@ -8275,7 +8275,7 @@ var _user$project$PhotoGroove$initialModel = {
 			_0: '9/11',
 			_1: {
 				ctor: '::',
-				_0: 'Cabal',
+				_0: 'Deep State',
 				_1: {
 					ctor: '::',
 					_0: 'New World Order',
@@ -8288,7 +8288,20 @@ var _user$project$PhotoGroove$initialModel = {
 			}
 		}
 	},
-	selectedTag: 'All'
+	selectedTag: 'All',
+	summaryPlaceholders: {
+		ctor: '::',
+		_0: {title: 'Conspiracy Title Placeholder', summary: 'Concept of the number one, rogue with pretty stories for which there\'s little good evidence from which we spring Hypatia a mote of dust suspended in a sunbeam hydrogen atoms take root and flourish gathered by gravity Hypatia! Vangelis extraplanetary made in the interiors of collapsing stars vanquish the impossible! Birth galaxies. Inconspicuous motes of rock and gas Tunguska event, Orion\'s sword trillion! Worldlets vastness is bearable only through love rich in heavy atoms as a patch of light tesseract and billions upon billions upon billions upon billions upon billions upon billions upon billions!'},
+		_1: {
+			ctor: '::',
+			_0: {title: 'Conspiracy Title Placeholder', summary: 'Emerged into consciousness intelligent beings, science the sky calls to us the ash of stellar alchemy laws of physics, dream of the mind\'s eye. Something incredible is waiting to be known billions upon billions decipherment not a sunrise but a galaxyrise descended from astronomers radio telescope concept of the number one muse about Euclid tesseract billions upon billions, preserve and cherish that pale blue dot intelligent beings tingling of the spine Sea of Tranquility Hypatia. Globular star cluster rich in mystery culture descended from astronomers ship of the imagination Apollonius of Perga. Birth Apollonius of Perga. Dispassionate extraterrestrial observer. How far away and billions upon billions upon billions upon billions upon billions upon billions upon billions!'},
+			_1: {
+				ctor: '::',
+				_0: {title: 'Conspiracy Title Placeholder', summary: 'Colonies, hydrogen atoms Flatland tingling of the spine quasar. Billions upon billions! As a patch of light decipherment consciousness permanence of the stars cosmic fugue brain is the seed of intelligence from which we spring astonishment science, take root and flourish explorations! Apollonius of Perga intelligent beings. Cambrian explosion? Consciousness, network of wormholes. Intelligent beings a mote of dust suspended in a sunbeam encyclopaedia galactica, Euclid laws of physics the only home we\'ve ever known a billion trillion? A mote of dust suspended in a sunbeam, great turbulent clouds Vangelis with pretty stories for which there\'s little good evidence brain is the seed of intelligence hearts of the stars and billions upon billions upon billions upon billions upon billions upon billions upon billions.'},
+				_1: {ctor: '[]'}
+			}
+		}
+	}
 };
 var _user$project$PhotoGroove$viewTag = F2(
 	function (selectedTag, tag) {
@@ -8323,6 +8336,64 @@ var _user$project$PhotoGroove$viewTag = F2(
 				_1: {ctor: '[]'}
 			});
 	});
+var _user$project$PhotoGroove$viewSummaries = function (summary) {
+	return A2(
+		_elm_lang$html$Html$div,
+		{
+			ctor: '::',
+			_0: _elm_lang$html$Html_Attributes$class('conspiracy-tease'),
+			_1: {ctor: '[]'}
+		},
+		{
+			ctor: '::',
+			_0: A2(
+				_elm_lang$html$Html$div,
+				{
+					ctor: '::',
+					_0: _elm_lang$html$Html_Attributes$class('conspiracy-title'),
+					_1: {ctor: '[]'}
+				},
+				{
+					ctor: '::',
+					_0: _elm_lang$html$Html$text(summary.title),
+					_1: {ctor: '[]'}
+				}),
+			_1: {
+				ctor: '::',
+				_0: A2(
+					_elm_lang$html$Html$div,
+					{
+						ctor: '::',
+						_0: _elm_lang$html$Html_Attributes$class('conspiracy-summary'),
+						_1: {ctor: '[]'}
+					},
+					{
+						ctor: '::',
+						_0: A2(
+							_elm_lang$html$Html$p,
+							{ctor: '[]'},
+							{
+								ctor: '::',
+								_0: _elm_lang$html$Html$text(summary.summary),
+								_1: {ctor: '[]'}
+							}),
+						_1: {ctor: '[]'}
+					}),
+				_1: {
+					ctor: '::',
+					_0: A2(
+						_elm_lang$html$Html$div,
+						{ctor: '[]'},
+						{
+							ctor: '::',
+							_0: _elm_lang$html$Html$text('View More Link Here'),
+							_1: {ctor: '[]'}
+						}),
+					_1: {ctor: '[]'}
+				}
+			}
+		});
+};
 var _user$project$PhotoGroove$view = function (model) {
 	return A2(
 		_elm_lang$html$Html$div,
@@ -8368,7 +8439,7 @@ var _user$project$PhotoGroove$view = function (model) {
 									_elm_lang$html$Html$div,
 									{
 										ctor: '::',
-										_0: _elm_lang$html$Html_Attributes$class('conspicies'),
+										_0: _elm_lang$html$Html_Attributes$class('content-heading'),
 										_1: {ctor: '[]'}
 									},
 									{
@@ -8384,7 +8455,14 @@ var _user$project$PhotoGroove$view = function (model) {
 											}),
 										_1: {ctor: '[]'}
 									}),
-								_1: {ctor: '[]'}
+								_1: {
+									ctor: '::',
+									_0: A2(
+										_elm_lang$html$Html$div,
+										{ctor: '[]'},
+										A2(_elm_lang$core$List$map, _user$project$PhotoGroove$viewSummaries, model.summaryPlaceholders)),
+									_1: {ctor: '[]'}
+								}
 							}),
 						_1: {ctor: '[]'}
 					}
